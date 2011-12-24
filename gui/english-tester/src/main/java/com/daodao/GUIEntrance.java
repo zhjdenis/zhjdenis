6e74441cf96024cad9c04093b54646c290ea2920
zhjdenis <zhjdenis@163.com>
2011-12-25 01:14:42 +0800

    folder structure

---
+++ gui/english-tester/src/main/java/com/daodao/GUIEntrance.java
@@ -0,0 +1,43 @@
+package com.daodao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GUIEntrance {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIEntrance window = new GUIEntrance();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUIEntrance() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ gui/english-tester/src/test/java/com/daodao/DAOTest.java
@@ -0,0 +1,14 @@
+package com.daodao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
