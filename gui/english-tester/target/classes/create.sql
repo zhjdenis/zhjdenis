746a7515a37f936b4e38f58934e5897ba5f27471
zhjdenis <zhjdenis@163.com>
2011-12-27 21:40:14 +0800

    remove classes

--- gui/english-tester/target/classes/create.sql
+++
@@ -1,4 +0,0 @@
-create table dictionary (id bigint not null generated always as identity, en varchar(1000), zh varchar(1000), type varchar(100),source varchar(100),accurate int)
-create table exam (id bigint not null generated always as identity, description varchar(1000), options varchar(1000), date DATE,remain int,correct int,wrong int)
-create table exam_word (id bigint not null generated always as identity, en varchar(1000), zh varchar(1000), type varchar(100),source varchar(100),examId bigint)
-create table history_exam_word (id bigint not null generated always as identity, en varchar(1000), zh varchar(1000), type varchar(100),source varchar(100),examId bigint)
