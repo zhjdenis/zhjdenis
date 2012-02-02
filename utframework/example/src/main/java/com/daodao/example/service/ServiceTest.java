6148b62c9df19ecd07ffb34a9a4de4873dd3af89
zhjdenis <zhjdenis@163.com>
2012-02-02 18:55:40 +0800

    refactor folder

---
+++ utframework/example/src/main/java/com/daodao/example/service/ServiceTest.java
@@ -0,0 +1,15 @@
+package com.daodao.example.service;
+
+import java.sql.Connection;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import com.daodao.framework.BaseTest;
+
+
+@PrepareForTest(value={})
+public class ServiceTest extends BaseTest
+{
+    protected Connection conn;
+    
+    }
